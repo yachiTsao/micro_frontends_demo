977cb1c22601687d66b6e212b515f8bb1324b896
yachiTsao <108506091+yachiTsao@users.noreply.github.com>
2025-04-08 09:53:52 +0000

    1.Install Vuetify 2. Create basic App.vue and Header.vue layout components

--- micro_frontends_demo/apps/host/src/main.ts
+++ micro_frontends_demo/apps/host/src/main.ts
@@ -1,10 +1,14 @@
-// import { createApp } from 'vue';
-// import App from './App.vue';
-
-// createApp(App).mount('#app');
-
+// filepath: /workspaces/micro_frontends_demo/micro_frontends_demo/apps/host/src/main.ts
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createVuetify } from "vuetify";
+import "vuetify/styles"; // 引入 Vuetify 樣式
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-const app = createApp(App);
-app.mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
